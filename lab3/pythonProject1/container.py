03bcf62204226cc9c34e77c7f470dcdb48bb8b0e
KarolinaaGrab <92163557+KarolinaaGrab@users.noreply.github.com>
2023-10-25 15:47:10 +0200

    Update container.py

--- lab3/pythonProject1/container.py
+++ lab3/pythonProject1/container.py
@@ -3,6 +3,7 @@ from dependency_injector import containers, providers
 from repositories.weather_repo_db import WeatherRepo
 from services.weather_service import WeatherService
 
+import os
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
@@ -18,4 +19,9 @@ class Container(containers.DeclarativeContainer):
     os.environ.get("s", "def")
 
     )
+    
+    if os.getenv("weather") == '0':
+        from repositories.weather_repo_db import WeatherRepo
+    else:
+        from repositories.weather_repo_txt import WeatherRepo
     )
